c1e51fb035af47e6cc439f4698071ea3493c3d0f
Keith Brandenburg <windweaver828@gmail.com>
2026-05-08 09:11:52 -0500

    Update simplifications, leaving lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,13 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require("core.options")
+require("core.keymaps")
+require("core.lsp")
+require("plugins")
+
+-- Optional local machine-specific config.
+-- Keep this untracked.
+local uv = vim.uv or vim.loop
+local local_config = vim.fn.stdpath("config") .. "/lua/local.lua"
+
+if uv.fs_stat(local_config) then
+  dofile(local_config)
+end
--- .config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Fix lazyvim using system clipboard as main register
-vim.opt.clipboard = ""
-vim.opt.fileformats = { "unix", "mac", "dos" }
